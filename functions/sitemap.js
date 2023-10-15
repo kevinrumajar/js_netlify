a0ba2af1a2b3ddc27ddbdc68f3a5287c6555506b
kevin <kevinrumajarzoom@gmail.com>
2023-10-15 18:51:13 +0700

    perbaiki rute firebase di functions/sitemap.js

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -1,15 +1,24 @@
 // fungsi generate-sitemap yang menghasilkan konten sitemap XML secara dinamis
-exports.handler = async (event, context) => {
-    const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <!-- Isi sitemap secara dinamis di sini -->
-    </urlset>`;
+const fetch = require('node-fetch');
 
-    return {
-        statusCode: 200,
-        body: sitemapContent,
-        headers: {
-            "Content-Type": "application/xml",
-        },
-    };
+exports.handler = async (event, context) => {
+    try {
+        // Ganti URL berikut dengan URL dari fungsi Firebase di firebase-functions/index.js
+        const firebaseFunctionURL = 'https://us-central1-auto-netlify.cloudfunctions.net/generateSitemap';
+        const response = await fetch(firebaseFunctionURL);
+        const sitemapContent = await response.text();
+
+        return {
+            statusCode: 200,
+            body: sitemapContent,
+            headers: {
+                "Content-Type": "application/xml",
+            },
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Error generating sitemap: ' + error,
+        };
+    }
 };
