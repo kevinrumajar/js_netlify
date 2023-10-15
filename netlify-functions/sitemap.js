e15740d9eca7679c77599d2ae367b58d52e12ef3
kevin <kevinrumajarzoom@gmail.com>
2023-10-15 23:37:12 +0700

    coba sitemap dengan netlify-functions

---
+++ netlify-functions/sitemap.js
@@ -0,0 +1,62 @@
+// netlify-functions/sitemap.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+// Konfigurasi Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBE3V28VDRZiDnRmbUMiyeS59qAe0vVsRQ",
+    authDomain: "auto-netlify.firebaseapp.com",
+    projectId: "auto-netlify",
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// mengambil kata kunci dari FB
+
+async function getAllKeywords() {
+    const keywordCollection = collection(db, 'keyword_collections');
+    const keywordSnapshot = await getDocs(keywordCollection);
+    return keywordSnapshot.docs.map(doc => doc.data().keyword);
+}
+
+// menghasilkan konten sitemap
+
+async function generateSitemap() {
+    const keywords = await getAllKeywords();
+    const sitemapEntries = keywords.map(keyword => {
+        const formattedKeyword = keyword.replace(/ /g, '-').toLowerCase();
+        const url = `https://(subdomain).netlify.app/${formattedKeyword}`;
+        return `
+<url>
+    <loc>${url}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.5</priority>
+</url>`;
+    }).join('\n');
+    return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${sitemapEntries}
+</urlset>`;
+}
+
+
+// Menangani Permintaan Fungsi:
+
+exports.handler = async function(event, context) {
+    try {
+        const sitemapContent = await generateSitemap();
+        return {
+            statusCode: 200,
+            headers: {"Content-Type": "application/xml"},
+            body: sitemapContent
+        };
+    } catch (error) {
+        console.error('Error generating sitemap:', error);
+        return {
+            statusCode: 500,
+            body: 'Internal Server Error'
+        };
+    }
+};
